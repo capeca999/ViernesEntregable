f28086bb5b441b571b3bd33b32cfb31028a69f34
capeca999 <47533906+capeca999@users.noreply.github.com>
2025-01-24 13:50:10 +0100

    Se conecta a la base de datos
    se registra el usuario con el postgre.

--- users/views.py
+++ users/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
+import psycopg2
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from config import load_config
+from connect import connect
 
 @csrf_exempt
 def login_view(request):
     if request.method == 'POST':
         try:
+            conn = connect()
+            load_config()
             data = json.loads(request.body)
             username = data.get('username')
             email = data.get('email')
@@ -17,21 +26,36 @@ def login_view(request):
             return response
         except json.JSONDecodeError:
             return HttpResponseBadRequest('Invalid JSON')
+        except Exception as e:
+            return HttpResponseBadRequest(f'Error: {str(e)}')
     return HttpResponseBadRequest('Invalid request method')
 
 @csrf_exempt
 def register_view(request):
     if request.method == 'POST':
         try:
+            conn = connect()
             data = json.loads(request.body)
             username = data.get('username')
             email = data.get('email')
             password = data.get('password')
             print(f"Received data: username={username}, email={email}, password={password}")
-            # Add your registration logic here
+            # Convert username to array format
+            username_array = '{' + username + '}'
+            email_array = '{' + email + '}'
+            password_array = '{' + password + '}'
+
+
+            sql = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
+            with conn.cursor() as cursor:
+                cursor.execute(sql, (username_array, email_array, password_array))
+                conn.commit()
+
             response = JsonResponse({'message': 'Registered successfully'})
             response['Access-Control-Allow-Methods'] = 'GET, POST, DELETE'
             return response
         except json.JSONDecodeError:
             return HttpResponseBadRequest('Invalid JSON')
+        except Exception as e:
+            return HttpResponseBadRequest(f'Error: {str(e)}')
     return HttpResponseBadRequest('Invalid request method')
