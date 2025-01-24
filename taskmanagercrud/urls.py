d02445322868c6d4b878cbb4b27578776121c141
capeca999 <47533906+capeca999@users.noreply.github.com>
2025-01-24 10:55:06 +0100

    JWT
    Creadas urls para jwt

--- taskmanagercrud/urls.py
+++ taskmanagercrud/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
-    path('', include('users.urls')),
+    path('users/', include ('django.contrib.auth.urls') ),
+    path('users/', include('users.urls')),
+
 ]
