e0047643743579c952fe0a8ac70679f1b78cea38
capeca999 <47533906+capeca999@users.noreply.github.com>
2025-01-24 12:12:24 +0100

    Fix some bugs

--- taskmanagercrud/urls.py
+++ taskmanagercrud/urls.py
@@ -5,7 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
-    path('users/', include ('django.contrib.auth.urls') ),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
 
 ]
